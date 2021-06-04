79bb06017cb2c8b7aeed88824a28d5638cf88efc
roman-yatsenko <roman.yatsenko@hneu.net>
2021-06-04 17:52:13 +0300

    Add start cmd

--- start.py
+++ start.py
@@ -4,9 +4,24 @@ from aiogram import Bot, Dispatcher, executor, types
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.reply(message.text)
+# Хэндлер на команду /start
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    poll_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    poll_keyboard.add(types.KeyboardButton(
+        text="Создать тест",
+        request_poll=types.KeyboardButtonPollType(type=types.PollType.QUIZ)
+    ))
+    poll_keyboard.add(types.KeyboardButton(text="Отмена"))
+    await message.answer("Нажмите на кнопку и создайте тест!", 
+        reply_markup=poll_keyboard)
+
+# Хэндлер на текстовое сообщение "Отмена"
+@dp.message_handler(lambda message: message.text == "Отмена")
+async def action_cancel(message: types.Message):
+    remove_keyboard = types.ReplyKeyboardRemove()
+    await message.answer("Действие отменено. Введите /start, чтобы начать заново",
+        reply_markup=remove_keyboard)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
