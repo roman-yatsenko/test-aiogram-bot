343d1307a53ee2908b23f9edaede98b0a7131cab
roman-yatsenko <roman.yatsenko@hneu.net>
2021-06-04 17:24:16 +0300

    Add echo bot

---
+++ start.py
@@ -0,0 +1,12 @@
+import config
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.reply(message.text)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
