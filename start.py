683fc06668356e69ee355f230aafa3085b07db34
roman-yatsenko <roman.yatsenko@hneu.net>
2021-06-04 18:37:39 +0300

    Add quiz saving

--- start.py
+++ start.py
@@ -27,5 +27,32 @@ async def action_cancel(message: types.Message):
     await message.answer("The action canceled. Type /start to launch again",
         reply_markup=remove_keyboard)
 
+@dp.message_handler(content_types=["poll"])
+async def msg_with_poll(message: types.Message):
+    user_id = str(message.from_user.id)
+    # if user is unknown
+    if not quiz_db.get(user_id):
+        quiz_db[user_id] = []
+
+    # quiz type check
+    if message.poll.type != "quiz":
+        await message.reply("Sorry, I can get only quizes!")
+        return
+
+    # quiz saving
+    quiz_db[user_id].append(Quiz(
+        quiz_id=message.poll.id,
+        question=message.poll.question,
+        options=[o.text for o in message.poll.options],
+        correct_option_id=message.poll.correct_option_id,
+        owner_id=user_id
+    ))
+    quiz_owners[message.poll.id] = user_id
+
+    await message.reply(
+        f"Quiz saved. Total quiz count: {len(quiz_db[user_id])}"
+    )
+
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
